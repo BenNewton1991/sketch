aed1982f2dd40734583dae4b5adb9c60d91179cf
Ben <bennewton91@gmail.com>
2022-09-19 16:33:46 +0100

    changed box size to fixed

--- script.js
+++ script.js
@@ -29,6 +29,40 @@ function add_squares () {
 }
 
 
+
+
+function custom_squares () {
+    a = prompt('enter number of shapes')
+    
+
+    grid.innerHTML = ''
+    let square = document.createElement('div');
+    square.classList.add('sqr');
+    square.style.width = 100/a;
+    square.style.height = 100/a;
+
+    num = a * a;
+
+    
+
+    
+
+
+    for (let i =0; i < num; i++){
+        let a = document.createElement('div');
+        a.classList.add('sqr');
+        a.addEventListener('mousedown', function (){
+            a.classList.add('sqr_hovered')
+        })
+        
+        grid.appendChild(a);
+    }
+
+}
+
+
+
+
 add_squares()
 
 function remove_squares () {
@@ -41,5 +75,5 @@ function remove_squares () {
 const btn_1 = document.querySelector('#new_grid');
 btn_1.addEventListener('click', remove_squares)
 
-const btn_2 = document.querySelector('#allow_draw');
-btn_2.addEventListener('click', add_squares)
+const btn_2 = document.querySelector('#custom_squares');
+btn_2.addEventListener('click', custom_squares)
